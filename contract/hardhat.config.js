9f82e052fbe150ffb9a0b04db356a383a81988d8
RonexLemon <ronexondimu@gmail.com>
2023-03-28 22:35:11 +0300

    implement configurations

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({path: ".env"});
+const KEY = process.env.KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
+  networks:{
+    alfajores:{
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: [KEY],
+      chainId: 44787,
+    },
+  },
 };
