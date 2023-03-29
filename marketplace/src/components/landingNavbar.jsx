39d37ba52bab5676d81c358205e7608fbd8e456d
RonexLemon <ronexondimu@gmail.com>
2023-03-29 23:24:58 +0300

    add navigation link

--- marketplace/src/components/landingNavbar.jsx
+++ marketplace/src/components/landingNavbar.jsx
@@ -5,10 +5,10 @@ const LandingNavBar= ()=>{
     return(
 
         <div className="h-20 w-full bg-black text-white flex justify-around items-center">
-            <div>
- <span>MarketPlace</span>
+            <div className="shadow-md shadow-orange-600">
+ <span>MotorS</span>
             </div>
-            <div className="bg-orange-300 rounded h-15  ">
+            <div className="shadow-md shadow-orange-600 h-15  ">
                 <button onClick={()=>{navigate("/marketplace")}} >Get Started</button>
                 </div>
 
--- marketplace/src/components/navbarallnfts.jsx
+++ marketplace/src/components/navbarallnfts.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 const NavBarAllNFTS= ()=>{
+    const navigate = useNavigate()
     return(
 
         <div className="h-20 w-full bg-black text-white flex justify-around items-center">
+             <div className="shadow-md shadow-orange-600">
+ <button onClick={()=>{navigate("/home")}}>Home</button>
+            </div>
             <div className="shadow-md shadow-orange-600">
  <button>MarketPlace</button>
             </div>
