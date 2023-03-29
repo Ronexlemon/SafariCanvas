8a82acca8f57b445517427cde2c3bb8effec91fd
RonexLemon <ronexondimu@gmail.com>
2023-03-29 23:21:56 +0300

    modify navigation bar

--- marketplace/src/components/navbarallnfts.jsx
+++ marketplace/src/components/navbarallnfts.jsx
@@ -4,10 +4,13 @@ const NavBarAllNFTS= ()=>{
     return(
 
         <div className="h-20 w-full bg-black text-white flex justify-around items-center">
-            <div>
- <span>MarketPlace</span>
+            <div className="shadow-md shadow-orange-600">
+ <button>MarketPlace</button>
             </div>
-            <div className="bg-orange-300 rounded h-15  ">
+            <div className=" h-15 shadow-md shadow-orange-600  ">
+                <button  >Mint NFT</button>
+                </div>
+            <div className=" h-15  shadow-md shadow-orange-600 ">
                 <button  >MY NFTS</button>
                 </div>
 
