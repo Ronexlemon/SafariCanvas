a4407c0040df960a78a0ab36f2d96b5a1982083f
RonexLemon <ronexondimu@gmail.com>
2023-03-30 21:04:00 +0300

    get access key

--- marketplace/src/components/mintnft.jsx
+++ marketplace/src/components/mintnft.jsx
@@ -1,8 +1,25 @@
-import React,{useState} from "react";
+import React,{useState,useContext} from "react";
 import jeep from "../assets/jeep.jpeg"
+import { AppContext } from "../../contexts/AppContexts";
+import { NFTABI } from "../abis/nftabi";
+import { NFTMinterAddress } from "../contractsadress/address";
+import { Web3Storage } from "web3.storage";
+const key = import.meta.env.KEY 
 
+const getAccessKey = ()=>{
+    return import .meta.env.VITE_KEY ;
+    
+}
+console.log("the key is",getAccessKey() );
 const Mintnftform= ()=>{
-    const [imageNft, setImageNft] = useState();
+     
+    const {
+        getProviderOrSigner,
+        Contract,
+    } = useContext(AppContext)
+    const [imageNft, setImageNft] = useState(jeep);
+    const [description,setDescription] = useState();
+    const [price,setPrice] = useState();
     const onChangePicture = e => {
         console.log('picture: ', e.target.files[0]);
         setImageNft(URL.createObjectURL(e.target.files[0]));
@@ -10,7 +27,7 @@ const Mintnftform= ()=>{
     return(
 
         <div className="h-screen w-full bg-black text-white ">
-<div class="w-full h-3/4  flex  justify-center rounded overflow-hidden shadow-lg  ">
+<div className="w-full h-3/4  flex  justify-center rounded overflow-hidden shadow-lg  ">
     <div className="shadow shadow-orange-700 grid grid-cols-1 gap-8 p-4">
         <h1>Create An NFT</h1>
         <img className="  max-h-60 max-w-md" src={imageNft} alt="image"/>
