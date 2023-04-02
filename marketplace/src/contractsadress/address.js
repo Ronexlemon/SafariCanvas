6ad8e3f87e4dc51256b9bac75d2e77b7f7f100a9
RonexLemon <ronexondimu@gmail.com>
2023-04-02 11:54:52 +0300

    change address and abi

--- marketplace/src/contractsadress/address.js
+++ marketplace/src/contractsadress/address.js
@@ -4,9 +4,12 @@
  //export const NFTMinterAddress =  "0x63851ce60983FB03CdCD101acC5545d0Aa69d72c"
 //export const  NFTMarketAddress =   "0x819995E53855898b2416293669c6F9F432E1Fc89"
 
-export const NFTMinterAddress =   "0xB1164aAd59554C372721C75c58c69591A614CF88"
-export const NFTMarketAddress =   "0xA330231Cbb7A7842fa3e8cBb308332416FF266b2"
+//export const NFTMinterAddress =   "0xB1164aAd59554C372721C75c58c69591A614CF88"
+//export const NFTMarketAddress =   "0xA330231Cbb7A7842fa3e8cBb308332416FF266b2"
 
 
 //export const NFTMinterAddress =   "0x7f6e7b295ccA9678F1c37a1CD087cbE81D556eaB"
 //export const NFTMarketAddress =   "0xBB04B52a2Eb3734632fD9B7e6fBd3E745198e83E"
+
+export const  NFTMinterAddress =   0xe7FcBd3996e911f516F42e1983ccd55aAeDF51EC
+export const  NFTMarketAddress =   0x09b55EFD8c7650701Dc98A0813638F398fB201Cf
